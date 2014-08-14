7e0c407ddc918387ad2946ff13f51e1e4d4c0615
Konstantin Ivanov <ivanov.konstantin@logrus.org.ru>
2014-08-15 03:45:51 +0400

    correct fizzbuzz

--- fizzbuzz/fizzbuzz.rb
+++ fizzbuzz/fizzbuzz.rb
@@ -3,8 +3,12 @@
 # fizzbuzz.rb
 
 (1..100).each do |i|
-	i = 'fizzbuzz' if i % 15 == 0
-	i = 'fizz' if i % 3 == 0
-	i = 'buzz' if i % 5 == 0
+	if i % 15 == 0
+		i = 'fizzbuzz'
+	elsif	i % 3 == 0
+		i = 'fizz'
+	elsif i % 5 == 0
+		i = 'buzz'
+	end
 	puts i
 end
